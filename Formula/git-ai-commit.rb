9a80ff9abe87fb3e09313863073579e320cf1466
Nagami Naoki <n_nagami@clickmaker.jp>
2025-01-03 09:49:25 +0900

    fix: description end stop

--- Formula/git-ai-commit.rb
+++ Formula/git-ai-commit.rb
@@ -1,5 +1,5 @@
 class GitAiCommit < Formula
-  desc "This script is a git subcommand that generates a commit message using ChatGPT."
+  desc "This script is a git subcommand that generates a commit message using ChatGPT"
   homepage "https://github.com/n-ngm/git-ai-commit"
   url "https://github.com/n-ngm/git-ai-commit/archive/refs/tags/v0.1.0.tar.gz"
   sha256 "7eb6aef9d31b550677e524b2ee75a11f7b31583e661110bccc65f55e3a8a7e0a"
